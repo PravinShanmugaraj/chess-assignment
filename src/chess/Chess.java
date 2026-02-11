aa949b9c30a3e9beffbfdfcf19f42fe3876ffb02
PravinShanmugaraj <pravinlinh@outlook.com>
2026-02-11 12:29:47 -0500

    added comments

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -47,15 +47,19 @@ public class Chess {
 		}
 	}
 
+	// creates a piece to be added based on its rank and file
 	private static ReturnPiece addPiece(int i, ReturnPiece.PieceFile pf) {
 
+		// create ReturnPiece object
 		ReturnPiece rp = new ReturnPiece();
 
+		// i corresponds to a rank: 0 -> 1, 1 -> 2, 2 -> 7, 3 -> 8
 		switch(i) {
 			case 0 -> {
 				rp.pieceRank = 1;
 				rp.pieceFile = pf;
 
+				// creates appropriate piece based on starting position
 				if(pf == ReturnPiece.PieceFile.a || pf == ReturnPiece.PieceFile.h) {
 					rp.pieceType = ReturnPiece.PieceType.WR;
 				}else if(pf == ReturnPiece.PieceFile.b || pf == ReturnPiece.PieceFile.g) {
@@ -73,6 +77,8 @@ public class Chess {
 			case 1 -> {
 				rp.pieceRank = 2;
 				rp.pieceFile = pf;
+
+				// all pieces in this row are pawns
 				rp.pieceType = ReturnPiece.PieceType.WP;
 
 				return rp;
@@ -80,6 +86,8 @@ public class Chess {
 			case 2 -> {
 				rp.pieceRank = 7;
 				rp.pieceFile = pf;
+
+				// all pieces in the row are pawns
 				rp.pieceType = ReturnPiece.PieceType.BP;
 
 				return rp;
@@ -88,6 +96,7 @@ public class Chess {
 				rp.pieceRank = 8;
 				rp.pieceFile = pf;
 
+				// creates appropriate piece based on starting position
 				if(pf == ReturnPiece.PieceFile.a || pf == ReturnPiece.PieceFile.h) {
 					rp.pieceType = ReturnPiece.PieceType.BR;
 				}else if(pf == ReturnPiece.PieceFile.b || pf == ReturnPiece.PieceFile.g) {
